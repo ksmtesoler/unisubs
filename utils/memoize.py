4881152f0cc124d7b9624910d3d569e0bd3aa01a
Ben Dean-Kawamura <ben@pculture.org>
2017-01-09 16:34:01 -0500

    Updated memoize.
    Now it works with functions with arguments and methods.  It can handle
    unhashable arguments if it's provided with a function that can convert
    them into a hash value.
    
    Added unittests.

--- utils/memoize.py
+++ utils/memoize.py
@@ -20,35 +20,66 @@
 
 import functools
 
-NOT_COMPUTED = object()
-
-class MemoStore(object):
-    """Helper class for memoize().
-
-    This provides some functionality, but the main thing it gets around is the
-    fact that python2.7 doesn't have the nonlocal keyword.
-    """
-    def __init__(self):
-        self.value = None
-        self.is_set = False
-
-    def set_value(self, value):
-        self.value = value
-        self.is_set = True
-
-def memoize(func):
+def memoize(func=None, key_func=None):
     """Memoize a function.
 
-    This wrapper remembers the return value of the first call.  On subsequent
-    calls, it simple returns it rather than calling the wrapped function
-    again.
+    We store the result of each call inside a dict.  If the function is called
+    with the same args, then we return the stored value instead of computing
+    it agin.
 
-    The wrapped function must take no arguments.
+    Memoize works with both functions and methods.
+
+    For this to work, we need to be able to compute a key for a function call.
+    By default this is simply the *args value.  This works if the function is
+    always called with hashable args and not with kwargs.  For more complex
+    calls, pass in a key function that generates a key based on the function
+    arguments.
     """
-    memo = MemoStore()
-    @functools.wraps(func)
-    def wrapper():
-        if not memo.is_set:
-            memo.set_value(func())
-        return memo.value
-    return wrapper
+    def decorator(func):
+        memoized_func = MemoizedFunction(func, key_func)
+        return memoized_func
+    # Kind of weird code, but it lets us either use the decorator directly, or
+    # after calling it with key_func
+    if key_func:
+        return decorator
+    else:
+        return decorator(func)
+
+class MemoizedFunction(object):
+    def __init__(self, func, key_func):
+        self.cache = {}
+        self.func = func
+        self.name = func.func_name
+        if key_func is None:
+            self.key_func = self.default_key_func
+        else:
+            self.key_func = key_func
+
+    def __call__(self, *args, **kwargs):
+        key = self.key_func(*args, **kwargs)
+        if key not in self.cache:
+            self.cache[key] = self.func(*args, **kwargs)
+        return self.cache[key]
+
+    def default_key_func(self, *args, **kwargs):
+        if kwargs:
+            raise TypeError("{} has been memoized and cannot accept kwargs.  "
+                            "Add a key_func if you want to use them".format(
+                                self.name))
+        return args
+
+    def clear_cache(self):
+        self.cache.clear()
+
+    # Implementing the descriptor protocol makes memoize() work with methods.
+    #
+    # When __get__ is called, we directly set the value in the object's
+    # __dict__ attribute.  This makes it so subsequent lookups skip __get__
+    # and get the MemoizedFunction already created.
+    def __get__(self, obj, type=None):
+        if obj is None:
+            return self
+        bound_method = self.func.__get__(obj)
+        memoized_func = MemoizedFunction(bound_method, self.key_func)
+        obj.__dict__[self.name] = memoized_func
+        return memoized_func
