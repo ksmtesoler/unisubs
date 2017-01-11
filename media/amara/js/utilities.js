e9966b1974d4c0c13447eef2b37833becaf42122
Ben Dean-Kawamura <ben@pculture.org>
2017-01-11 16:27:43 -0500

    CSS changes from occupop

--- media/amara/js/utilities.js
+++ media/amara/js/utilities.js
@@ -45,3 +45,40 @@ var urlParams;
        urlParams[decode(match[1])] = decode(match[2]);
 })();
 
+
+// Check visibility - This utility is in place for helping with hidden content.
+(function ($) {
+    var defaults = {
+        callback: function () { },
+        runOnLoad: true,
+        frequency: 100,
+        previousVisibility : null
+    };
+
+    var methods = {};
+    methods.checkVisibility = function (element, options) {
+        if (jQuery.contains(document, element[0])) {
+            var previousVisibility = options.previousVisibility;
+            var isVisible = element.is(':visible');
+            options.previousVisibility = isVisible;
+            if (previousVisibility == null) {
+                if (options.runOnLoad) {
+                    options.callback(element, isVisible);
+                }
+            } else if (previousVisibility !== isVisible) {
+                options.callback(element, isVisible);
+            }
+
+            setTimeout(function() {
+                methods.checkVisibility(element, options);
+            }, options.frequency);
+        } 
+    };
+
+    $.fn.visibilityChanged = function (options) {
+        var settings = $.extend({}, defaults, options);
+        return this.each(function () {
+            methods.checkVisibility($(this), settings);
+        });
+    };
+})(jQuery);
