c83ead8bc62c3445fcba1eb45ce0bb4b6bc189a0
Ben Dean-Kawamura <ben@pculture.org>
2017-01-27 20:36:54 -0500

    Pulled in some changes from amara-design

--- media/amara/js/fileUpload.js
+++ media/amara/js/fileUpload.js
@@ -23,6 +23,7 @@
 $.behaviors('.fileUpload', fileUpload);
 
 function fileUpload(select) {
+  
     $(document).on('change', ':file', function() {
       var input = $(this),
           numFiles = input.get(0).files ? input.get(0).files.length : 1,
