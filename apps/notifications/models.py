0dbb3543e554610c61b2a067b3c4ca935e1469ce
Sylvain Chevalier <sylvain@pculture.org>
2017-02-11 21:43:11 +0100

    Fixed bad variable name

--- apps/notifications/models.py
+++ apps/notifications/models.py
@@ -46,9 +46,9 @@ class TeamNotificationSettings(models.Model):
         try:
             return TeamNotificationSettings.objects.get(team=team)
         except TeamNotificationSettings.DoesNotExist:
-            teams = TeamNotificationSettings.objects.filter(extra_teams=team)
-            if teams.exists():
-                return teams[0]
+            team_settings = TeamNotificationSettings.objects.filter(extra_teams=team)
+            if team_settings.exists():
+                return team_settings[0]
             return None
 
     def get_headers(self):
