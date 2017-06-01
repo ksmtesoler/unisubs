63929e8f5d3be4e315475ef093fa2d020a557949
amelia sabine <amelia.sabine@riseup.net>
2017-06-01 13:42:36 -0500

    redirect on error to new-style page for collab teams

--- apps/subtitles/views.py
+++ apps/subtitles/views.py
@@ -373,6 +373,8 @@ class SubtitleEditorBase(View):
         if (not self.check_can_edit() or
             not self.check_can_writelock() or
             not self.assign_task_for_editor()):
+            if self.team_video:
+                return redirect(self.video.get_absolute_url() + "?team=" + self.team_video.team.slug)
             return redirect(self.video)
 
         self.editing_language.writelock(self.user, self.request.browser_id,
