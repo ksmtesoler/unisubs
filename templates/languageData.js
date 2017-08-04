d20ec41f2ede04ae5dde58f0ae724529916f4cb6
Sylvain Chevalier <sylvain@pculture.org>
2017-08-04 16:44:59 +0200

    Fixed language selector when language name has non-ascii characters

--- templates/languageData.js
+++ templates/languageData.js
@@ -1,7 +1,7 @@
 {% load i18n %}
 var allLanguages = [ {% for code, name in languages %}"{{ code }}"{% if not forloop.last %},{% endif %}{% endfor %} ];
 var popularLanguages = [{% for code in popular_languages %}"{{ code }}"{% if not forloop.last %},{% endif %}{% endfor %} ];
-var languageNames = { {% for code, name in languages %}"{{ code }}": "{{ name }}"{% if not forloop.last %},{% endif %}{% endfor %} };
+var languageNames = { {% for code, name in languages %}"{{ code }}": "{{ name|safe }}"{% if not forloop.last %},{% endif %}{% endfor %} };
 var localeChoices = { {% for code in locale_choices %}"{{ code }}":1{% if not forloop.last %},{% endif %}{% endfor %} };
 
 var allLanguagesLabel = {{ allLanguagesLabel|safe }};
