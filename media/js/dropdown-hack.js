6f2d2c5bdefdf557f689c13d5b7773cd6076c270
Ben Dean-Kawamura <ben@pculture.org>
2017-08-28 11:53:05 -0400

    Close dropdown when clicking outside it

--- media/js/dropdown-hack.js
+++ media/js/dropdown-hack.js
@@ -1,13 +1,31 @@
 $(function() {
-    $('.dropdown-toggle').click(function() {
+    $('.dropdown-toggle').each(function() {
         var link = $(this);
         var dropdown = link.closest('.dropdown');
-        if(dropdown.hasClass('open')) {
-            dropdown.removeClass('open');
-            link.attr("aria-expanded", "false");
-        } else {
+
+        function openMenu() {
             dropdown.addClass('open');
             link.attr("aria-expanded", "true");
         }
+        function closeMenu() {
+            dropdown.removeClass('open');
+            link.attr("aria-expanded", "false");
+        }
+        link.click(function(evt) {
+            if(dropdown.hasClass('open')) {
+                closeMenu();
+            } else {
+                openMenu();
+            }
+            evt.preventDefault();
+            return false;
+        });
+        $('body').click(function(evt) {
+            if(dropdown.hasClass('open') && !dropdown.has(evt.target)) {
+                closeMenu();
+                evt.preventDefault();
+                return false;
+            }
+        });
     });
 });
