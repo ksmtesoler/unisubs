85a766cc7c7ad48c21afa92297b0c666793a3c97
Sylvain Chevalier <sylvain@pculture.org>
2017-10-26 22:35:43 +0200

    Workaround for Vimeo player issue

--- apps/videos/types/vimeo.py
+++ apps/videos/types/vimeo.py
@@ -72,7 +72,10 @@ class VimeoVideoType(VideoType):
                 pass
     
     def _get_vimeo_id(self, video_url):
-        return vimeo.VIMEO_REGEX.match(video_url).groupdict().get('video_id') 
+        return vimeo.VIMEO_REGEX.match(video_url).groupdict().get('video_id')
+
+    def player_url(self):
+        return self.get_direct_url() or self.url
 
     def get_direct_url(self, prefer_audio=False):
         r = requests.get("https://player.vimeo.com/video/{}/config".format(self.video_id))
