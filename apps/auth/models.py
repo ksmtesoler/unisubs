08a04622ddac620d3552b312a238f457f47f92af
amelia sabine <amelia.sabine@riseup.net>
2017-12-13 10:47:42 -0500

    set default as callable directly

--- apps/auth/models.py
+++ apps/auth/models.py
@@ -858,7 +858,7 @@ class LoginToken(models.Model):
 class AmaraApiKey(models.Model):
     user = models.OneToOneField(CustomUser, related_name="api_key")
     created = models.DateTimeField(auto_now_add=True)
-    key = models.CharField(max_length=256, blank=True, default=lambda: generate_api_key())
+    key = models.CharField(max_length=256, blank=True, default=generate_api_key)
 
     def __unicode__(self):
         return u"Api key for {}: {}".format(self.user, self.key)
